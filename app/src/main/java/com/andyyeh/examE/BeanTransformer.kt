d3ce0d8017fbaca98048f8ee2af2e0db7b79c52b
andyyeh <andy82115@gmail.com>
2020-06-11 23:20:57 +0800

    make BeanTransformer to convert netData to local

---
+++ app/src/main/java/com/andyyeh/examE/BeanTransformer.kt
@@ -0,0 +1,24 @@
+package com.andyyeh.examE
+
+import android.util.Log
+import com.andyyeh.examE.mainActivity.model.UserBean
+import com.example.gituserapi.NetUserBean
+
+class BeanTransformer private constructor(){
+
+    companion object {
+        val instance = BeanTransformer()
+    }
+
+    fun netUserBean2UserBean(netUserBeans: List<NetUserBean>): ArrayList<UserBean>{
+        val userBeans = ArrayList<UserBean>()
+        for (netUserBean in netUserBeans){
+            val userBean = UserBean(
+                netUserBean.avatar_url,
+                netUserBean.login,
+                netUserBean.site_admin)
+            userBeans.add(userBean)
+        }
+        return userBeans
+    }
+}
--- app/src/main/java/com/andyyeh/examE/mainActivity/MainActivityRepository.kt
+++ app/src/main/java/com/andyyeh/examE/mainActivity/MainActivityRepository.kt
@@ -1,14 +1,18 @@
 package com.andyyeh.examE.mainActivity
 
+import android.annotation.SuppressLint
 import android.util.Log
+import com.andyyeh.examE.BeanTransformer
 import com.andyyeh.examE.di.BasicInfo
 import com.andyyeh.examE.mainActivity.model.UserBean
 import com.example.andynovelapi.internetApi.UserApiMethods
 import com.example.andynovelapi.internetApi.UserObserver
 import com.example.andynovelapi.internetApi.UserObserverListener
-import com.example.climateapi.Configuration
 import com.example.gituserapi.NetUserBean
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
 
 class MainActivityRepository(basicInfo: BasicInfo) : MainContract.Repository{
 
@@ -17,8 +21,15 @@ class MainActivityRepository(basicInfo: BasicInfo) : MainContract.Repository{
 
     override fun getDataFromInternet(since: Int, page: Int, consumer: Consumer<ArrayList<UserBean>>) {
         val onNextListener = object : UserObserverListener.ObserverOnNextListener<List<NetUserBean>>{
+            @SuppressLint("CheckResult")
             override fun onNext(t: List<NetUserBean>) {
-                if (Configuration.DEBUG) Log.e(TAG, t.toString())
+                Single.just(t)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .map(fun(netUserBeans: List<NetUserBean>) : ArrayList<UserBean> {
+                        return BeanTransformer.instance.netUserBean2UserBean(netUserBeans)
+                    })
+                    .subscribe(consumer)
             }
         }
 
